7d062bd6f43e42fa1f6577b3fecfe62f05649236
GioMonci <95672640+GioMonci@users.noreply.github.com>
2026-07-06 17:41:49 -0400

    remove empty files and add main script with initial docstring

---
+++ src/main.py
@@ -0,0 +1,17 @@
+"""
+File: main.py
+Author: Gio Monci
+Updated: 07/06/2026
+
+Description:
+    Brief description of what this script does.
+"""
+
+def main() -> None:
+    """ Main entry point. """
+    
+
+if __name__ == "__main__":
+    main()
+
+    
